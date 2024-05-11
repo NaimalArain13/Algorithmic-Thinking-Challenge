3b2da875e3d56a7e14a3f9249e26573fc26ea679
Naimal Salahuddin <naimalarain13@gmail.com>
2024-05-12 02:28:33 +0500

    day 01 tasks added

---
+++ Day-01/main.js
@@ -0,0 +1,8 @@
+"use strict";
+// I challenge you to tackle a simple algorithmic problem, such as finding the sum of elements in an array. Share your approach and insights in the comments to foster learning and collaboration.
+let sum = 0;
+let array = [1, 2, 3, 4];
+for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+}
+console.log(sum);
---
+++ Day-01/main.ts
@@ -0,0 +1,9 @@
+// I challenge you to tackle a simple algorithmic problem, such as finding the sum of elements in an array. Share your approach and insights in the comments to foster learning and collaboration.
+
+let array:number[] = [1,2,3,4]
+let sum = 0;
+for(let i = 0 ; i < array.length ; i++){
+    
+    sum = sum + array[i] 
+}
+console.log(sum);
