43ac76f93568668116f13a27b8bd01dd450f2fd7
NaimalArain13 <naimalarain13@gmail.com>
2024-08-10 23:23:06 +0500

    added

--- Day-13/main.ts
+++ Day-13/main.ts
@@ -81,8 +81,8 @@ function findMissingNumber(arr: number[]): number{
         let actualSum = 0
         for(let i=0;i<arr.length;i++){
             actualSum += arr[i]
-        }
 
+        }
         return expectedSum - actualSum
 }
 
